45f6804a823d30c85f2829708a4e70284175b2e6
Nicolas Kumabe <nicolas.kumabe@gmail.com>
2023-12-18 19:34:38 -0300

    Implementando load

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  defaultCommandTimeout: 10000,
+  pageLoadTimeout: 50000,
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here

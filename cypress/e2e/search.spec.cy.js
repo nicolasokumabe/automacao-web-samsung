6bdb2df53f807a1098cdf86ea10992d4844ac889
Nicolas Kumabe <nicolas.kumabe@gmail.com>
2023-12-20 11:29:50 -0300

    Dispositivo no carrinho com sucesso

--- cypress/e2e/search.spec.cy.js
+++ cypress/e2e/search.spec.cy.js
@@ -24,19 +24,27 @@ describe('home page', () => {
         cy.get('ul li div input[an-la="storage:256 gb"] + label span span span').should('have.text', 'R$ 4.499,00')
         cy.get('ul li div input[an-la="storage:128 gb"] + label span span span').should('not.have.text')
 
-        // cy.get('span a[aria-label="Comprar agora:Galaxy S23 FE"]').click()
-
+        // ignorar token
         cy.on('uncaught:exception', (err, runnable) => {
             console.log(err);
             return false;
         });
 
+        // clicar no botão comprar
         cy.get('span a[aria-label="Comprar agora:Galaxy S23 FE"]').should('have.text', 'Comprar agora').click()
-        cy.contains('continuar sua compra')
-        cy.wait(20000)
-        // cy.visit('https://www.samsung.com/br/smartphones/galaxy-s/galaxy-s23-fe-mint-128gb-sm-s711blgjzto/') 
-        // // clica no botao de compra
+        // cy.contains('continuar sua compra')
+
+        // confere a existência do produto pela classe
         cy.get('div[class="vtex-flex-layout-0-x-flexRow vtex-flex-layout-0-x-flexRow--page-product-info"]')
-        cy.get('.samsungbr-app-pdp-2-x-inputPostalCode').type('01310930')
+
+        cy.get('input[placeholder="Digite o CEP"]', { timeout: 60000 }).type('01310930')
+        cy.get('#buttonPostalCodePdp').click()
+
+        // clica em comprar
+        cy.get('div[class="vtex-flex-layout-0-x-flexColChild vtex-flex-layout-0-x-flexColChild--product-content-buy-info pb0"] button[type="button"] span')
+            .should('have.text', 'COMPRAR AGORA').click({ force: true })
+
+        // verifica se estou na pagina de carrinho
+        cy.url().should('eq', 'https://shop.samsung.com/br/checkout/#/cart');
     })
 })
